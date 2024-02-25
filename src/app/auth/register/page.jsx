016247ee085ed05f3ebfeb33ad1f8576fef1a1d1
Yeison Rojas <yeisonjr98@gmail.com>
2024-02-25 17:35:26 -0400

    fix fetch register

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -15,7 +15,7 @@ const RegisterPage = () => {
 
     if (password !== confirmPassword) return alert('passsword not match')
     
-    const res = await fetch('http://localhost:3000/api/auth/register',{
+    const res = await fetch('/api/auth/register',{
       method: 'POST',
       body: JSON.stringify({
         username,
